d514d52c347eed047e6ba5d779d30f2cd874e3e3
EgorexW <olko.wiktor.10@gmail.com>
2026-08-04 20:55:36 +0200

    Refactor campaign state management: rename fields in mission and combat prefabs, add Main Campaign State Holder asset, and update initialization logic

--- Assets/Scripts/Others/Init.cs
+++ Assets/Scripts/Others/Init.cs
@@ -3,14 +3,12 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class Init : MonoBehaviour{
-    [BoxGroup("References")] [Required] [SerializeField] CampaignStateHolder campaignStateHolder;
     [BoxGroup("References")] [Required] [SerializeField] SaveSystem saveSystem;
 
     [SerializeField] [SceneObjectsOnly] protected string sceneName;
 
     protected void Awake(){
-        var campaignState = saveSystem.Load();
-        campaignStateHolder.SetCampaignState(campaignState);
+        saveSystem.Load();
         SceneManager.LoadScene(sceneName);
     }
 }
