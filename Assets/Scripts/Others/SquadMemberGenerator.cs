efc9a24a131eb732f8fc76e4512a27ab1cdc7bbb
EgorexW <olko.wiktor.10@gmail.com>
2026-08-05 19:52:52 +0200

    Add SquadMemberGenerator: implement recruit generation logic and define RecruitTemplate structure

---
+++ Assets/Scripts/Others/SquadMemberGenerator.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SquadMemberGenerator : ScriptableObject{
+    [SerializeField] List<RecruitTemplate> possibleTemplates = new();
+    
+    
+    public void GenerateRecruits(int amount){
+        for (int i = 0; i < amount; i++){
+            GenerateRecruit();
+        }
+    }
+
+    void GenerateRecruit(){
+        string randomName = NameGenerator.RandomName();
+        RecruitTemplate randomTemplate = possibleTemplates.Random();
+    
+        SquadMember newRecruit = new SquadMember(
+            randomName, 
+            randomTemplate.combatPrefab, 
+            new List<Equipment>(randomTemplate.startingEquipment),
+            randomTemplate.upkeepCost
+        );
+    }
+}
+
+[Serializable]
+public struct RecruitTemplate{
+    public GameObject combatPrefab;
+    public List<Equipment> startingEquipment;
+    public int upkeepCost;
+}
+
+    }
